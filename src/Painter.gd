1d3add9e85b47205f53e0533484b8fb1b73966e9
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2026-08-29 22:20:56 +0200

    move square painting to the painter

--- src/Painter.gd
+++ src/Painter.gd
@@ -1,6 +1,8 @@
 class_name Painter
 
 const SW = Config.SQUARE_WIDTH
+const BW = Config.SQUARE_BORDER_WIDTH
+const IW = SW - (BW * 2)
 
 static func drawGuide(canvas: CanvasItem, rect: Rect2) -> void:
   canvas.draw_rect(rect, Colors.UI_BACKGROUND)
@@ -19,3 +21,32 @@ static func drawGuide(canvas: CanvasItem, rect: Rect2) -> void:
     canvas.draw_line(
       Vector2(lineX, rect.position.y), Vector2(lineX, rect.end.y), Colors.UI_GUIDE
     )
+
+static func drawSquareAtPosition(canvas: CanvasItem, position: Vector2, color: Color) -> void:
+  canvas.draw_rect(Rect2(position, Vector2(SW, SW)), color)
+  var p := position
+  # left border
+  canvas.draw_polygon(
+    PackedVector2Array([p, p + Vector2(BW, BW), p + Vector2(BW, SW - BW), p + Vector2(0, SW)]),
+    PackedColorArray([Colors.SQUARE_BORDER_SIDE])
+  )
+  # right border
+  canvas.draw_polygon(
+    PackedVector2Array(
+      [p + Vector2(SW, 0), p + Vector2(SW - BW, BW), p + Vector2(SW - BW, SW - BW), p + Vector2(SW, SW)]
+    ),
+    PackedColorArray([Colors.SQUARE_BORDER_SIDE])
+  )
+
+  # top border
+  canvas.draw_polygon(
+    PackedVector2Array([p, p + Vector2(BW, BW), p + Vector2(SW - BW, BW), p + Vector2(SW, 0)]),
+    PackedColorArray([Colors.SQUARE_BORDER_TOP])
+  )
+  # bottom border
+  canvas.draw_polygon(
+    PackedVector2Array(
+      [p + Vector2(0, SW), p + Vector2(BW, SW - BW), p + Vector2(SW - BW, SW - BW), p + Vector2(SW, SW)]
+    ),
+    PackedColorArray([Colors.SQUARE_BORDER_BOTTOM])
+  )
--- src/screens/game/components/Square.gd
+++ src/screens/game/components/Square.gd
@@ -1,44 +1,11 @@
 class_name Square
-extends Polygon2D
 
-static var SW = Config.SQUARE_WIDTH
-static var BW = Config.SQUARE_BORDER_WIDTH
-static var IW = SW - (BW * 2)
-
+const SW = Config.SQUARE_WIDTH
+const BW = Config.SQUARE_BORDER_WIDTH
+const IW = SW - (BW * 2)
 
 static func create(cell: Vector2i, color: Color) -> Square:
-	var square := Square.new()
-	square.polygon = PackedVector2Array(
-		[Vector2(0, 0), Vector2(SW, 0), Vector2(SW, SW), Vector2(0, SW)]
-	)
-	square.position = Vector2(cell.x * SW, cell.y * SW)
-	square.color = color
-	return square
-
-
-func _draw():
-	# left border
-	draw_polygon(
-		PackedVector2Array([Vector2(0, 0), Vector2(BW, BW), Vector2(BW, SW - BW), Vector2(0, SW)]),
-		PackedColorArray([Colors.SQUARE_BORDER_SIDE])
-	)
-	# right border
-	draw_polygon(
-		PackedVector2Array(
-			[Vector2(SW, 0), Vector2(SW - BW, BW), Vector2(SW - BW, SW - BW), Vector2(SW, SW)]
-		),
-		PackedColorArray([Colors.SQUARE_BORDER_SIDE])
-	)
-
-	# top border
-	draw_polygon(
-		PackedVector2Array([Vector2(0, 0), Vector2(BW, BW), Vector2(SW - BW, BW), Vector2(SW, 0)]),
-		PackedColorArray([Colors.SQUARE_BORDER_TOP])
-	)
-	# bottom border
-	draw_polygon(
-		PackedVector2Array(
-			[Vector2(0, 0 + SW), Vector2(BW, SW - BW), Vector2(SW - BW, SW - BW), Vector2(SW, SW)]
-		),
-		PackedColorArray([Colors.SQUARE_BORDER_BOTTOM])
-	)
+  var square := Square.new()
+  square.position = Vector2(cell.x * SW, cell.y * SW)
+  square.color = color
+  return square
