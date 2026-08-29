363cde36e47affeab54b0074d1e5f6cf8d59fe96
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2026-08-29 20:39:34 +0200

    no need for Square scene

--- src/screens/game/components/Square.gd
+++ src/screens/game/components/Square.gd
@@ -1,11 +1,22 @@
+class_name Square
 extends Polygon2D
 
 static var SW = Config.SQUARE_WIDTH
 static var BW = Config.SQUARE_BORDER_WIDTH
 static var IW = SW - (BW * 2)
 
+
+static func create(cell: Vector2i, color: Color) -> Square:
+	var square := Square.new()
+	square.polygon = PackedVector2Array(
+		[Vector2(0, 0), Vector2(SW, 0), Vector2(SW, SW), Vector2(0, SW)]
+	)
+	square.position = Vector2(cell.x * SW, cell.y * SW)
+	square.color = color
+	return square
+
+
 func _draw():
-	#draw_rect(Rect2(0, 0, Config.SQUARE_WIDTH, Config.SQUARE_WIDTH), color)
 	# left border
 	draw_polygon(
 		PackedVector2Array([Vector2(0, 0), Vector2(BW, BW), Vector2(BW, SW - BW), Vector2(0, SW)]),
