83b0b96dc267cc55d34b396668c7db16d70fd4ff
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2026-08-29 20:02:17 +0200

    colors should be constants

--- src/Colors.gd
+++ src/Colors.gd
@@ -1,23 +1,23 @@
 extends Node
 
-var TETROMINO_CYAN = Color("#00F0F0FF")
-var TETROMINO_YELLOW = Color("#F0F000FF")
-var TETROMINO_PURPLE = Color("#A000F0FF")
-var TETROMINO_BLUE = Color("#0000F0FF")
-var TETROMINO_ORANGE = Color("#F0A000FF")
-var TETROMINO_GREEN = Color("#00F000FF")
-var TETROMINO_RED = Color("#F00000FF")
+const TETROMINO_CYAN = Color("#00F0F0FF")
+const TETROMINO_YELLOW = Color("#F0F000FF")
+const TETROMINO_PURPLE = Color("#A000F0FF")
+const TETROMINO_BLUE = Color("#0000F0FF")
+const TETROMINO_ORANGE = Color("#F0A000FF")
+const TETROMINO_GREEN = Color("#00F000FF")
+const TETROMINO_RED = Color("#F00000FF")
 
-var SQUARE_DEFAULT_COLOR = Color("#cc8081FF")
-var SQUARE_BORDER_TOP = Color("#FFFFFF99")  # White at 61% Opacity
-var SQUARE_BORDER_BOTTOM = Color("#00000080")  # Black at 50% Opacity
-var SQUARE_BORDER_SIDE = Color("#0000001A")  # Black at 10% Opacity
+const SQUARE_DEFAULT_COLOR = Color("#cc8081FF")
+const SQUARE_BORDER_TOP = Color("#FFFFFF99")  # White at 61% Opacity
+const SQUARE_BORDER_BOTTOM = Color("#00000080")  # Black at 50% Opacity
+const SQUARE_BORDER_SIDE = Color("#0000001A")  # Black at 10% Opacity
 
-var UI_BACKGROUND = Color("#333333FF")
-var UI_SIDEBAR_BACKGROUND = Color("#545454FF")
-var UI_POPUP_BACKGROUND = Color("#212121FF")
-var UI_BUTTON_BACKGROUND = TETROMINO_CYAN
-var UI_GUIDE = Color("#555555FF")
-var UI_WHITE_TEXT = Color("#FFFFFFFF")
-var UI_POPUP_TEXT = Color("#EFEFEFFF")
-var UI_BUTTON_TEXT = Color("#212121FF")
+const UI_BACKGROUND = Color("#333333FF")
+const UI_SIDEBAR_BACKGROUND = Color("#545454FF")
+const UI_POPUP_BACKGROUND = Color("#212121FF")
+const UI_BUTTON_BACKGROUND = TETROMINO_CYAN
+const UI_GUIDE = Color("#555555FF")
+const UI_WHITE_TEXT = Color("#FFFFFFFF")
+const UI_POPUP_TEXT = Color("#EFEFEFFF")
+const UI_BUTTON_TEXT = Color("#212121FF")
