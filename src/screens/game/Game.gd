4dfa6d2cecf4e8353cfcb07a6bb035e469d829f1
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2026-08-29 21:49:19 +0200

    test drawGrid

--- src/screens/game/Game.gd
+++ src/screens/game/Game.gd
@@ -2,12 +2,16 @@ extends Node2D
 
 
 func _ready() -> void:
-	var squares = [
-		Square.create(Vector2i(0, 0), Colors.TETROMINO_CYAN),
-		Square.create(Vector2i(1, 0), Colors.TETROMINO_CYAN),
-		Square.create(Vector2i(2, 0), Colors.TETROMINO_CYAN),
-		Square.create(Vector2i(3, 0), Colors.TETROMINO_CYAN),
-	]
+  # var squares = [
+  # 	Square.create(Vector2i(0, 0), Colors.TETROMINO_CYAN),
+  # 	Square.create(Vector2i(1, 0), Colors.TETROMINO_CYAN),
+  # 	Square.create(Vector2i(2, 0), Colors.TETROMINO_CYAN),
+  # 	Square.create(Vector2i(3, 0), Colors.TETROMINO_CYAN),
+  # ]
 
-	for s in squares:
-		add_child(s)
+  # for s in squares:
+  # 	add_child(s)
+  pass
+
+func _draw() -> void:
+  Painter.drawGuide(self, Rect2(Vector2(Config.SIDEBAR_WIDTH, 0), Vector2(Config.WAR_ZONE_WIDTH, Config.CANVAS_HEIGHT)))
