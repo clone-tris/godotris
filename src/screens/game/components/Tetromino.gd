38bee5a70b91cfb453d4e89583ca0c4845b264c4
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2026-09-03 18:45:58 +0200

    Tetromino init

---
+++ src/screens/game/components/Tetromino.gd
@@ -0,0 +1,42 @@
+class_name Tetromino
+
+enum Type {
+  I,
+  O,
+  T,
+  J,
+  L,
+  S,
+  Z,
+}
+
+const GRIDS: Array[Array] = [
+  [[0, 0], [0, 1], [0, 2], [0, 3]], # I
+  [[0, 0], [0, 1], [1, 0], [1, 1]], # O
+  [[0, 0], [0, 1], [0, 2], [1, 1]], # T
+  [[0, 0], [1, 0], [1, 1], [1, 2]], # J
+  [[0, 0], [0, 1], [0, 2], [1, 0]], # L
+  [[0, 1], [0, 2], [1, 0], [1, 1]], # S
+  [[0, 0], [0, 1], [1, 1], [1, 2]], # Z
+]
+
+const COLORS: Array[Color] = [
+  Colors.TETROMINO_CYAN,
+  Colors.TETROMINO_YELLOW,
+  Colors.TETROMINO_PURPLE,
+  Colors.TETROMINO_BLUE,
+  Colors.TETROMINO_ORANGE,
+  Colors.TETROMINO_GREEN,
+  Colors.TETROMINO_RED,
+]
+
+
+static func makeSquares(type: Type) -> Array[Square]:
+  var squares: Array[Square] = []
+  var shapeColor := COLORS[type]
+  var grid: Array[Array] = GRIDS[type]
+
+  for coords: Array[int] in grid:
+    squares.append(Square.new(coords[0], coords[1], shapeColor))
+
+  return squares
