a21acdd3f4e0b0b0675548204ada090113818a18
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2026-09-08 18:21:27 +0200

    general button styles

--- src/screens/over/Over.gd
+++ src/screens/over/Over.gd
@@ -1,6 +1,4 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _on_retry_button_pressed() -> void:
+	print("hi")
