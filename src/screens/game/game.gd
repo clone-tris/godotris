b2504fc59e6939b9d186780c421e0656bc8feae3
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2026-09-08 19:45:39 +0200

    restore close on Q in game

--- src/screens/game/game.gd
+++ src/screens/game/game.gd
@@ -145,8 +145,7 @@ func _draw() -> void:
 
 func _input(event: InputEvent) -> void:
   if event.is_action_pressed("Quit", true):
-    state = State.GAME_OVER
-    # commandQueue.append(Command.CLOSE)
+    commandQueue.append(Command.CLOSE)
   if event.is_action_pressed("Restart", true):
     commandQueue.append(Command.RESTART)
 
