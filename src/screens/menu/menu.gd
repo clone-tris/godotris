fecef3355c2ec9a6e9d03fbb37d33135387fc2e0
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2026-09-08 19:13:31 +0200

    rename scripts to be lowercase

--- src/screens/menu/menu.gd
+++ src/screens/menu/menu.gd
@@ -1,10 +1,9 @@
-extends Control
+extends Node2D
 
-func _on_retry_button_pressed() -> void:
+
+func _on_start_button_pressed() -> void:
   get_tree().change_scene_to_file("res://src/screens/game/Game.tscn")
 
-func _on_menu_button_pressed() -> void:
-  print("go menu")
 
 func _on_quit_button_pressed() -> void:
   get_tree().quit()
@@ -12,5 +11,5 @@ func _on_quit_button_pressed() -> void:
 func _input(event: InputEvent) -> void:
   if event.is_action_pressed("Quit", true):
     get_tree().quit()
-  if event.is_action_pressed("Restart", true):
+  if event.is_action_pressed("Start", true):
     get_tree().change_scene_to_file("res://src/screens/game/Game.tscn")
---
+++ src/screens/over/over.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _on_retry_button_pressed() -> void:
+  get_tree().change_scene_to_file("res://src/screens/game/Game.tscn")
+
+func _on_menu_button_pressed() -> void:
+  get_tree().change_scene_to_file("res://src/screens/menu/Menu.tscn")
+
+func _on_quit_button_pressed() -> void:
+  get_tree().quit()
+
+func _input(event: InputEvent) -> void:
+  if event.is_action_pressed("Quit", true):
+    get_tree().quit()
+  if event.is_action_pressed("Restart", true):
+    get_tree().change_scene_to_file("res://src/screens/game/Game.tscn")
