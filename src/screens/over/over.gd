ab8729b5a2676d1a2a9459b1ffbf117d9d107a41
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2026-09-08 19:14:52 +0200

    go to meny from game over

--- src/screens/over/over.gd
+++ src/screens/over/over.gd
@@ -10,7 +10,9 @@ func _on_quit_button_pressed() -> void:
   get_tree().quit()
 
 func _input(event: InputEvent) -> void:
-  if event.is_action_pressed("Quit", true):
-    get_tree().quit()
   if event.is_action_pressed("Restart", true):
     get_tree().change_scene_to_file("res://src/screens/game/Game.tscn")
+  if event.is_action_pressed("Menu", true):
+    get_tree().change_scene_to_file("res://src/screens/menu/Menu.tscn")
+  if event.is_action_pressed("Quit", true):
+    get_tree().quit()
