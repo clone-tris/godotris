9e77da4bb190ac9060fb2a75b2e29678e016db3c
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2026-09-08 18:28:25 +0200

    quit or restart from over

--- src/screens/over/Over.gd
+++ src/screens/over/Over.gd
@@ -1,4 +1,10 @@
 extends Control
 
 func _on_retry_button_pressed() -> void:
-	print("hi")
+  get_tree().change_scene_to_file("res://src/screens/game/Game.tscn")
+
+func _on_menu_button_pressed() -> void:
+  print("go menu")
+
+func _on_quit_button_pressed() -> void:
+  get_tree().quit()
